510eaf67a57beeb82a6402c4f15277919d0d5522
nahum-nieves <anahumnieves@gmail.com>
2026-02-10 14:50:04 -0600

    First program

---
+++ program-1/main.go
@@ -0,0 +1,13 @@
+/*
+Package main is the entry point of the program. 
+It contains the main function which is the starting point of execution.
+*/
+package main
+
+/*
+The main function is the entry point of the program. 
+the function name must be main and it must not take any arguments and must not return any value.
+*/
+func hello()  {
+	
+}
