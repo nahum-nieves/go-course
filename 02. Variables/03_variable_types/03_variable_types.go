ac654c534dabc9d228783b2456a265eda7b561b8
nahum-nieves <anahumnieves@gmail.com>
2026-02-13 15:20:06 -0600

    Add variable types demonstration in new file

---
+++ 02. Variables/03_variable_types/03_variable_types.go
@@ -0,0 +1,15 @@
+package main
+
+import "fmt"
+
+func main() {
+	// Integer types
+	name := "Alice"
+	age := 30
+	height := 1.75
+	var isMarried bool = false
+	fmt.Printf("Type of name is %T\n", name)
+	fmt.Printf("Type of age is %T\n", age)
+	fmt.Printf("Type of height is %T\n", height)
+	fmt.Printf("Type of isMarried is %T\n", isMarried)
+}
