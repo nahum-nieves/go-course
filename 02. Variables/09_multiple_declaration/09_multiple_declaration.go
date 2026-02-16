35a82c4c60e2b81092abae58646a0014a3266973
nahum-nieves <anahumnieves@gmail.com>
2026-02-16 14:52:08 -0600

    Add multiple variable declaration demonstration in new file

---
+++ 02. Variables/09_multiple_declaration/09_multiple_declaration.go
@@ -0,0 +1,9 @@
+package main
+import "fmt"
+
+func main(){
+	var name, age, isStudent = "Alice", 30, true;
+	fmt.Println("Name:", name)
+	fmt.Println("Age:", age)
+	fmt.Println("Is Student:", isStudent)
+}
