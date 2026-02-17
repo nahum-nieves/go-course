e69c77a4647b24d2ee3349ec5b848b6919641f40
nahum-nieves <anahumnieves@gmail.com>
2026-02-17 14:16:24 -0600

    Add grouped variable declarations demonstration in new file

---
+++ 02. Variables/12_grouped_declarations/12_grouped_declarations.go
@@ -0,0 +1,15 @@
+package main
+import "fmt"
+
+func main() {
+	var (
+		name      string  = "Alice"
+		age       int     = 30
+		isStudent bool    = true
+		gpa       float64 = 3.8
+	)
+	fmt.Println("Name:", name)
+	fmt.Println("Age:", age)
+	fmt.Println("Is Student:", isStudent)
+	fmt.Println("GPA:", gpa)
+}
