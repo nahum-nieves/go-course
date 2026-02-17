9e18f751ef51ba2500287f1fc96eb5e4c18255e6
nahum-nieves <anahumnieves@gmail.com>
2026-02-17 15:25:37 -0600

    Refactor greet function to use a constant for greeting message

--- Functions/02_parameters/02_parameters.go
+++ Functions/02_parameters/02_parameters.go
@@ -1,11 +1,13 @@
 package main
 import "fmt"
 
+const message = "Hello,"
 // The greet function takes a string parameter called name and prints a personalized greeting message.
 func greet(name string) {
-	fmt.Println("Hello,", name)
+	fmt.Println(message, name)
 }
 func main() {
+	
 	//Here we are calling the greet function with different arguments to print personalized greetings.
 	greet("Alice")
 	greet("Bob")
